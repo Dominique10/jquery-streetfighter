4657476a8589bcb71a291d1066dc27c348d4637c
Dominique10 <Dominiquey@ymail.com>
2016-02-23 17:02:44 -0500

    Added Hadouken sound

---
+++ js/app.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+		$('.ryu-cool').hide();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		$('ryu-cool').hide();
+	})
+	.mousedown(function(){
+		//console.log('mousedown');
+		//play hadouken sound
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '520px');
+    }
+  );
+		//show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function(){
+		//console.log('mouseup');
+		//ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+	  $(document).keydown(function(x) {
+    if (x.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(x) {
+    if (x.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+    }
+  });
+
+});
+
+
+
+
+
+
+function playHadouken(){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
